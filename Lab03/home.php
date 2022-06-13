b334d83552618d00740165c1c8476426c12f12e7
TanvirHasanMisho <106055372+TanvirHasanMisho@users.noreply.github.com>
2022-06-13 13:32:38 +0600

    lab 03

---
+++ Lab03/home.php
@@ -0,0 +1,28 @@
+<?php 
+	session_start();
+   
+
+	if(isset($_SESSION['status'])){
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home Page</title>
+</head>
+<body>
+		<h1>Welcome Home!</h1>
+		<a href="logout.php"> logout </a>
+</body>
+</html>
+
+<?php 
+$email = $_SESSION['email'];
+
+} else{
+
+	echo "invalid request";
+} ?>
---
+++ Lab03/login.php
@@ -0,0 +1,19 @@
+<?php 
+	session_start();
+
+	$username = $_REQUEST['username'];
+	$password = $_REQUEST['password'];
+
+	if ($username == null || $password == null) {
+		echo "invalid username/password <br>";
+	}else{
+		if($username == $password){
+			$_SESSION['status'] = true;
+			header('location: home.php');
+		}else{
+			echo "invalid user";
+		}
+	}
+
+
+?>
